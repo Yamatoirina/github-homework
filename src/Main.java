c2d7a453cb63c15087ead6d8a6c3e7e1e6f48eac
Yamatoirina <yamatoirina@gmail.com>
2021-03-03 16:42:01 +0200

    [HW-02] added checks for parameters. Changed method modifiers to private.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+
+        if (args.length != 0) {
+            print(args[0]);
+            try {
+                print(args[0], Integer.parseInt(args[1]));
+            } catch (ArrayIndexOutOfBoundsException | NumberFormatException e) {
+                System.out.println("Second parameter should be a number");
+            }
+        } else {
+            System.out.println("You should enter some text, or text and number");
+        }
     }
 
 
-    public static void print(String str) {
+    private static void print(String str) {
         System.out.println(str);
     }
 
 
-    public static void print(String str, int num) {
+    private static void print(String str, int num) {
         for (int i = 0; i < num; i++) {
             System.out.println(str);
         }
